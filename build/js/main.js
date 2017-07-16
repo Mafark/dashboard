0c3f8f4afac620bb83ee690695bdb39ef06f95b8
Никита Яблоков <danger@torman.ru>
2017-07-17 01:30:04 +0300

    delete bootstrap, add header, sidebar

--- build/js/main.js
+++ build/js/main.js
@@ -1,27 +1,29 @@
 $(document).ready(function () {
-  $('.h-name-container').removeClass('hidden');
-  $('.h-name-char').removeClass('h-name-char-hidden');
-  $('.h-profession').removeClass('h-profession-hidden');
-});
+  jQuery('img.svg').each(function () {
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
 
-$(window).scroll(function () {
-  var gearDeg = $(this).scrollTop();
-  $('.gear-1').css({
-    'transform': ' translateX(-110%) rotate(' + gearDeg/5 + 'deg)'
+    jQuery.get(imgURL, function (data) {
+      // Get the SVG tag, ignore the rest
+      var $svg = jQuery(data).find('svg');
+
+      // Add replaced image's ID to the new SVG
+      if (typeof imgID !== 'undefined') {
+        $svg = $svg.attr('id', imgID);
+      }
+      // Add replaced image's classes to the new SVG
+      if (typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass + ' replaced-svg');
+      }
+
+      // Remove any invalid XML tags as per http://validator.w3.org
+      $svg = $svg.removeAttr('xmlns:a');
+
+      // Replace image with new SVG
+      $img.replaceWith($svg);
+
+    }, 'xml');
   });
-  $('.gear-2').css({
-    'transform': ' translateX(-10%) rotate(-' + gearDeg/5 + 'deg)'
-  });
-  $('.gear-3').css({
-    'transform': ' translateX(-110%) rotate(' + gearDeg/5 + 'deg)'
-  });
-  $('.gear-4').css({
-    'transform': ' translateX(110%) rotate(-' + gearDeg/5 + 'deg)'
-  });
-  $('.gear-5').css({
-    'transform': ' translateX(10%) rotate(' + gearDeg/5 + 'deg)'
-  });
-  $('.gear-6').css({
-    'transform': ' translateX(110%) rotate(-' + gearDeg/5 + 'deg)'
-  });
-});
+})
