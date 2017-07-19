96a30334a34c8ec526b933d5f3801b36d2456b1c
Никита Яблоков <danger@torman.ru>
2017-07-19 23:52:57 +0300

    edit change tab function, add animation

--- build/js/main.js
+++ build/js/main.js
@@ -1,23 +1,62 @@
 'use strict';
 $(document).ready(function() {
-  $('.server').load('../content/sidebarMenu/server.html');
-  $().initializeMenu('.sidebar-tabs', '.sidebar-tabs-content', '../content/sidebarMenu/', 'server');
+  $().initializeMenu('.sidebar-tabs', '.sidebar-tabs-content', '../content/serverState.html');
   preloaderToggle();
 });
 
+var CONSTANT = {
+  site: ''
+};
 (function($) {
-  $.fn.initializeMenu = function(menuSelector, contentSelector, path, firstTab, callback) {
-    $(contentSelector).load(path + firstTab + '.html');
+  $.fn.initializeMenu = function(menuSelector, contentSelector, firstContent, callback) {
+    getContent(contentSelector, firstContent);
     $(menuSelector + ' li a').click(function() {
-      var linkName = $(this).attr('href');
-      var link = path + linkName + '.html';
-      $(contentSelector).load(link);
+      var link = CONSTANT.site + $(this).attr('href');
+      getContent(contentSelector, link);
       $(menuSelector + ' li').removeClass('active');
       $(this).parent().addClass('active');
       callback ? callback() : null;
+      history.pushState('', '', link);
       return false;
     });
   };
+
+  var getContent = function(contentSelector, link) {
+    preloaderToggle();
+    setTimeout(function() { //DELETE timeout
+      $(contentSelector).fadeOut('fast', function() {
+        $(contentSelector).load(link, function() {
+          preloaderToggle();
+          $(contentSelector).fadeIn('fast');
+        });
+      });
+    }, 2000);
+  };
+
+  // var insertContent = function (contentSelector, htmlContent) {
+  //   $(contentSelector).html(htmlContent)
+  // }
+
+  // var getTab = function (contentName, contentDataUrl, callback) {
+  //   var response = {
+  //     content: null,
+  //     data: null
+  //   };
+  //   ajax.getTabContent(contentName).then(function (htmlContent) {
+  //     response.content = htmlContent;
+  //     (response.data) {
+  //       callback();
+  //       return response;
+  //     } : null;
+  //   })
+  //   ajax.getTabData(contentDataUrl).then(function (data) { //server/1
+  //     response.data = data;
+  //     (response.content) {
+  //       callback();
+  //       return response;
+  //     } : null;
+  //   })
+  // }
 })(jQuery);
 
 (function($) {
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 $(document).ready(function() {
-  $('.server').load('../content/sidebarMenu/server.html');
-  $().initializeMenu('.sidebar-tabs', '.sidebar-tabs-content', '../content/sidebarMenu/', 'server');
+  $().initializeMenu('.sidebar-tabs', '.sidebar-tabs-content', '../content/serverState.html');
   preloaderToggle();
 });
