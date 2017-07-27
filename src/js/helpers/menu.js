7b0918a613187b4b48e8cc312d08fad2b86177c8
Никита Яблоков <danger@torman.ru>
2017-07-27 16:13:17 +0300

    add replace active class button

--- src/js/helpers/menu.js
+++ src/js/helpers/menu.js
@@ -12,8 +12,20 @@
   });
 })();
 
+//dropdown toggle
 (function() {
   $('.dropdown-tab').children('a').click(function(params) {
     $(this).siblings('ul').slideToggle('medium');
+  });
+})();
+
+//replace active class
+(function() {
+  var links = $('.sidebar-menu a[href]');
+  links.click(function(event) {
+    links.each(function(index, link) {
+      $(link).parent('li').removeClass('active');
+    });
+    $(this).parent('li').addClass('active');
   });
 })();
