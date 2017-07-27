09085e88b6dcd5c8635129370f403b9ad1f18f4b
Никита Яблоков <danger@torman.ru>
2017-07-27 17:35:21 +0300

    refactor

--- build/js/main.js
+++ build/js/main.js
@@ -133,37 +133,37 @@ var CONSTANT = {
       }
     });
   };
-
-  var createSocket = function(url, message, onMessage) {
-    var socket;
-    var connected = false;
-
-    typeof WebSocket !== 'undefined' &&
-      (function connect() {
-        socket = new WebSocket(url);
-        socket.onmessage = onMessage;
-        socket.onopen = () => {
-          connected = true;
-          setInterval(function() {
-            socket.send('{ "command": "' + message + '" }');
-          }, 1000);
-        };
-        socket.onerror = err => {
-          console.error(err);
-          socket.onclose = null;
-          connected = false;
-          socket.close();
-          connect();
-        };
-        socket.onclose = event => {
-          console.info(`WebSocket closed with code ${event.code}! ${event.reason}`);
-          connected = false;
-          if (event.wasClean) return;
-          connect();
-        };
-      })();
-  };
 })();
+
+var createSocket = function(url, message, onMessage) {
+  var socket;
+  var connected = false;
+
+  typeof WebSocket !== 'undefined' &&
+    (function connect() {
+      socket = new WebSocket(url);
+      socket.onmessage = onMessage;
+      socket.onopen = () => {
+        connected = true;
+        setInterval(function() {
+          socket.send('{ "command": "' + message + '" }');
+        }, 1000);
+      };
+      socket.onerror = err => {
+        console.error(err);
+        socket.onclose = null;
+        connected = false;
+        socket.close();
+        connect();
+      };
+      socket.onclose = event => {
+        console.info(`WebSocket closed with code ${event.code}! ${event.reason}`);
+        connected = false;
+        if (event.wasClean) return;
+        connect();
+      };
+    })();
+};
 
 $(window).on('changeUrl', function(event, data) {
   svgReplace();
